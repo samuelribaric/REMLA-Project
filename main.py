e3af1905629067af509a320de9e5b52dee4ae9d7
Samuel Ribaric <samuel.ribaric@live.se>
2024-05-01 16:39:44 +0200

    Update main.py

--- main.py
+++ main.py
@@ -11,15 +11,15 @@ from sklearn.preprocessing import LabelEncoder
 from sklearn.metrics import classification_report, confusion_matrix
 
 # Load data
-train = [line.strip() for line in open("/kaggle/input/dl-dataset/DL Dataset/train.txt", "r").readlines()[1:]]
+train = [line.strip() for line in open("data/train.txt", "r").readlines()[1:]]
 raw_x_train = [line.split("\t")[1] for line in train]
 raw_y_train = [line.split("\t")[0] for line in train]
 
-test = [line.strip() for line in open("/kaggle/input/dl-dataset/DL Dataset/test.txt", "r").readlines()]
+test = [line.strip() for line in open("data/test.txt", "r").readlines()]
 raw_x_test = [line.split("\t")[1] for line in test]
 raw_y_test = [line.split("\t")[0] for line in test]
 
-val = [line.strip() for line in open("/kaggle/input/dl-dataset/DL Dataset/val.txt", "r").readlines()]
+val = [line.strip() for line in open("data/val.txt", "r").readlines()]
 raw_x_val = [line.split("\t")[1] for line in val]
 raw_y_val = [line.split("\t")[0] for line in val]
 
