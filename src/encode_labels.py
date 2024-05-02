08d23d476cef4b6b71c943f321bc7fecfc8984a0
Daniel Chou <choudaniel3@gmail.com>
2024-05-02 21:25:22 +0200

    revert batch processing

--- src/encode_labels.py
+++ src/encode_labels.py
@@ -4,23 +4,17 @@ from sklearn.preprocessing import LabelEncoder
 
 # Daniel: We are using batch processing, because else I run into memory issues.
 
-def encode_labels(input_path, output_path, batch_size=10000):
+def encode_labels(input_path, output_path):
+    # Load labels from the file
     with open(input_path, 'r') as file:
-        labels = []
-        encoder = LabelEncoder()
-        # Open the output file once and append to it
-        with open(output_path, 'ab') as output_file:  # Using 'ab' to append in binary mode
-            for line in file:
-                labels.append(line.strip())
-                if len(labels) >= batch_size:
-                    # Process the current batch
-                    encoded_labels = encoder.fit_transform(labels)
-                    np.savetxt(output_file, encoded_labels, fmt='%d')
-                    labels = []
-            # Process any remaining labels
-            if labels:
-                encoded_labels = encoder.fit_transform(labels)
-                np.savetxt(output_file, encoded_labels, fmt='%d')
+        labels = [line.strip() for line in file]
+
+    # Encode labels
+    encoder = LabelEncoder()
+    encoded_labels = encoder.fit_transform(labels)
+
+    # Save the encoded labels
+    np.savetxt(output_path, encoded_labels, fmt='%d')
 
 
 def main():
